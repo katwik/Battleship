236a6ae50bd6d3a2d063ddf69c290dc751b03051
katwik <>
2023-08-04 11:13:45 +0000

    fix bugs

--- run.py
+++ run.py
@@ -1,31 +1,28 @@
-#Random module for randomly accepting the values
-# ‘X’ indicates the ships hit
-# ‘-‘ indicates the hits missed
 from random import randint
 
-Hidden_Pattern=[[' ']*9 for x in range(9)]
-Guess_Pattern=[[' ']*9 for x in range(9)]
+Hidden_Board=[[' ']*9 for x in range(9)]
+Guess_Board=[[' ']*9 for x in range(9)]
 
 letters_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8}
 
 def print_board(board):
-    print(' A B C D E F G H I')
-    print(' +---------------+')
+    print("  A B C D E F G H I")
+    print(" +-----------------+")
     row_num=1
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num +=1
 
 def get_ship_location():
-    row=input('Please enter a ship row 1-8 ')
-    while row not in '12345678':
+    row=input('Please enter a ship row 1-9 ')
+    while row not in '123456789':
         print("Please enter a valid row ")
-        row=input('Please enter a ship row 1-8 ')
-    column=input('Please enter a ship column A-H ')
-    while column not in 'ABCDEFGH':
+        row=input('Please enter a ship row 1-9 ')
+    column=input('Please enter a ship column A-I ')
+    while column not in 'ABCDEFGHI':
         print("Please enter a valid column ")
-        column=input('Please enter a ship column A-H ')
-    return int(row)-1,let_to_num[column]
+        column=input('Please enter a ship column A-I ')
+    return int(row)-1,letters_to_num[column]
 
 #Function that creates the ships
 def create_ships(board):
@@ -45,27 +42,26 @@ def count_hit_ships(board):
                 count+=1
     return count
 
-create_ships(Hidden_Pattern)
-#print_board(Hidden_Pattern)
-turns = 10
+create_ships(Hidden_Board)
+turns = 15
 while turns > 0:
-    print('Welcome to Battleship')
-    print_board(Guess_Pattern)
+    print('Welcome to Battleship!')
+    print_board(Guess_Board)
     row,column =get_ship_location()
-    if Guess_Pattern[row][column] == '-':
+    if Guess_Board[row][column] == '-':
         print(' You already guessed that ')
-    elif Hidden_Pattern[row][column] =='X':
-        print(' Congratulations you have hit the battleship ')
-        Guess_Pattern[row][column] = 'X'
+    elif Hidden_Board[row][column] =='X':
+        print(' Congratulations, you made a hit! ')
+        Guess_Board[row][column] = 'X'
         turns -= 1
     else:
-        print('Sorry,You missed')
-        Guess_Pattern[row][column] = '-'
+        print('Sorry, You missed')
+        Guess_Board[row][column] = '-'
         turns -= 1
-    if  count_hit_ships(Guess_Pattern) == 5:
+    if  count_hit_ships(Guess_Board) == 5:
         print("Congratulations you have sunk all the battleships ")
         break
     print(' You have ' +str(turns) + ' turns remaining ')
     if turns == 0:
         print('Game Over ')
-        break
+        
