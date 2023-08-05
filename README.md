2c58e51143fc43e25fcb671808d2502bf027425e
katwik <>
2023-08-05 07:40:29 +0000

    add readme

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Battleships
-    Developer Katarina Wikström
 
 This Battleships is a Python terminal game, that runs on Heroku in a mock terminal provided by Code Institute.
 The user tries to find the battleships in 15 turns. Each ship occupies one square on the board.
@@ -9,12 +8,34 @@ Battleships is based on the classic pen-and-paper game.
 In this version the computer creates a hidden board with five ships and one empty board the player sees. The user then tries to find the ships by guessing a row and a column. Guesses are marked on the board by - . Hits are marked with an X. The game is over when the user has made 15 guesses or found all the ships before the countdown.
 
 ## Features
-* Random board generation
-    * Ships are randomly placed on a hidden board which the user can not see. 
-* User input
-* 
+* Ships are randomly placed on a hidden board which the user can not see. 
+* Accepts user input
+* You cannot enter coordinates outside the grid
+* You cannot guess the same location twice
+* Turn counts
 
 ### Future features
 * The player can choose the size of the board
 * The computer is part of the game and guesses the ship locations of the player
 * The player can choose the ship locations 
+
+## Testing
+I manually tested this game by doing the following:
+* Run the code through CI Python Linter, with no errors to show
+* Tested the game in my local terminal and the Heroku terminal
+* Given invalid coordinates and made the same guess twice
+
+## Deployment
+* Create an account and login into Heroku website
+* Click "New -> Create new app" button
+* Insert your app's Name and Choose your region then click the "Create App" button
+* Into the Settings tab go to "Config vars" section and click "Reveal Config Vars"
+* Enter the PORT in the KEY section and 8000 for its value, then click "Add"
+* Go to "Buildpacks" section and click "Add buildpack"
+* Firstly add the Python buildpack then NodeJs
+* Into the Deploy tab go to "Deployment method" and select Github
+* After that go to "App connected to GitHub" and look for your GitHub repository name to link it
+* You can now choose to either manually or automatically deploy your app to Heroku.
+* With automatic deploys enabled, Heroku will build a new version of the app each time a change has been pushed to the repository
+* Manual deploys means your app will be updated only when you manually click to deploy it
+* When the deploying is finished, a link will be provided to you for accessing your app
--- run.py
+++ run.py
@@ -1,56 +1,70 @@
 from random import randint
 
-Hidden_Board=[[' ']*9 for x in range(9)]
-Guess_Board=[[' ']*9 for x in range(9)]
+Hidden_Board = [[' ']*9 for x in range(9)]
+Guess_Board = [[' ']*9 for x in range(9)]
 
-letters_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8}
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8
+}
+
 
 def print_board(board):
     print("  A B C D E F G H I")
     print(" +-----------------+")
-    row_num=1
+    row_num = 1
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
-        row_num +=1
+        row_num += 1
+
 
 def get_ship_location():
-    row=input('Please enter a ship row 1-9 ')
+    row = input('Please enter a ship row 1-9 ')
     while row not in '123456789':
         print("Please enter a valid row ")
-        row=input('Please enter a ship row 1-9 ')
-    column=input('Please enter a ship column A-I ')
+        row = input('Please enter a ship row 1-9 ')
+    column = input('Please enter a ship column A-I ')
     while column not in 'ABCDEFGHI':
         print("Please enter a valid column ")
-        column=input('Please enter a ship column A-I ')
-    return int(row)-1,letters_to_num[column]
+        column = input('Please enter a ship column A-I ')
+    return int(row) - 1, letters_to_num[column]
 
-#Function that creates the ships
+# Function that creates the ships
+
+
 def create_ships(board):
     for ship in range(5):
-        ship_r, ship_cl=randint(0,7), randint(0,7)
-        while board[ship_r][ship_cl] =='X':
+        ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        while board[ship_r][ship_cl] == 'X':
             ship_r, ship_cl = randint(0, 7), randint(0, 7)
         board[ship_r][ship_cl] = 'X'
 
 
-
 def count_hit_ships(board):
-    count=0
+    count = 0
     for row in board:
         for column in row:
-            if column=='X':
-                count+=1
+            if column == 'X':
+                count += 1
     return count
+
 
 create_ships(Hidden_Board)
 turns = 15
 while turns > 0:
     print('Welcome to Battleship!')
     print_board(Guess_Board)
-    row,column =get_ship_location()
+    row, column = get_ship_location()
     if Guess_Board[row][column] == '-':
         print(' You already guessed that ')
-    elif Hidden_Board[row][column] =='X':
+    elif Hidden_Board[row][column] == 'X':
         print(' Congratulations, you made a hit! ')
         Guess_Board[row][column] = 'X'
         turns -= 1
@@ -58,10 +72,9 @@ while turns > 0:
         print('Sorry, You missed')
         Guess_Board[row][column] = '-'
         turns -= 1
-    if  count_hit_ships(Guess_Board) == 5:
+    if count_hit_ships(Guess_Board) == 5:
         print("Congratulations you have sunk all the battleships ")
         break
-    print(' You have ' +str(turns) + ' turns remaining ')
+    print(' You have ' + str(turns) + ' turns remaining ')
     if turns == 0:
         print('Game Over ')
-        
